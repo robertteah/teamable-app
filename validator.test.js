843dd95db8f4450cdc89a506d445312861463c33
Robert Teah <robertteah@gmail.com>
2023-10-30 19:58:32 -0400

    added server integration testing

--- validator.test.js
+++ validator.test.js
@@ -1,4 +1,4 @@
-const { isInvalidEmail } = require('./validator')
+const { isInvalidEmail, isEmptyPayload } = require('./validator')
 
 test('valid email', function() {
           const testPayload = {
@@ -7,4 +7,31 @@ test('valid email', function() {
                     interest: "testng"
           }
           const result = isInvalidEmail(testPayload)
-          expect(result).toBe(false)          
+          expect(result).toBe(false)
+})
+
+test('invalid email', function() {
+          const testPayload = {
+                    name: "John Doe",
+                    email: "test.email",
+                    interest: "testng"
+          }
+          const result = isInvalidEmail(testPayload)
+          expect(result).toBe(true)
+})
+
+test('empty payload', function() {
+          const testPayload = {}
+          const result = isEmptyPayload(testPayload)
+          expect(result).toBe(true)
+})
+
+test('non-empty payload', function() {
+          const testPayload = {
+                    name: "test name", 
+                    email: "test.email@example.com",
+                    interest: "testng"
+          }
+          const result = isEmptyPayload(testPayload)
+          expect(result).toBe(false)
+})
